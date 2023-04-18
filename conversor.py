b8a34e494c3f5b85611ad3e20eebd5c5e0ce5225
jvsobraz <jvsobraz@hotmail.com>
2023-04-18 20:47:16 -0300

    Conversor

---
+++ conversor.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+import os
+
+texto = "Olá, mundo! Este é um exemplo de conversão de texto para voz usando Python."
+
+tts = gTTS(text=texto, lang='pt')
+
+tts.save("exemplo.mp3")
+
+os.system("mpg321 exemplo.mp3")  # Substitua "mpg321" pelo player de áudio correto para o seu sistema operacional
